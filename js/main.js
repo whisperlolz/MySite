e4e2e52309a6f58d999489f8cceab333dfc054cf
WhisperloLapt <kyrylokondrashov@gmail.com>
2020-10-21 13:34:43 +0200

    about/projects

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+
 'use strict';
 
-var welcome = document.querySelector('.welcome');
-var jumpCursor  = welcome.querySelector('.welcome_cloud_text')
+(function () {
 
-jumpCursor.classList('hidden');
+    var header = document.querySelector('header');
+    var headerContent = header.querySelector('.header_content');
+    var aboutMe = document.querySelector('.aboutMe');
+    var skills = document.querySelector('.skills');
+    var skillsText = skills.querySelector('.skills_text');
+
+
+        $(window).scroll(function () {
+            var long = $(window).scrollTop();
+            if (long > headerContent.offsetTop+5) {
+                headerContent.classList.add('header_fixed');
+                console.log(skills.offsetTop + skills.offsetHeight);
+            } else {
+                headerContent.classList.remove('header_fixed');
+            }
+            if (long + aboutMe.offsetHeight >= aboutMe.offsetTop + aboutMe.offsetHeight ) {
+                 skillsText.classList.add('skills_text_animation');
+            }
+        });
+    
+})();
