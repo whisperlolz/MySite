9ebf1c21c0719acae191a2d1b4c63b5c958b7c45
WhisperloLapt <kyrylokondrashov@gmail.com>
2020-10-22 12:33:01 +0200

    footer/window

--- js/main.js
+++ js/main.js
@@ -3,18 +3,19 @@
 
 (function () {
 
-    var header = document.querySelector('header');
-    var headerContent = header.querySelector('.header_content');
-    var aboutMe = document.querySelector('.aboutMe');
-    var skills = document.querySelector('.skills');
-    var skillsText = skills.querySelector('.skills_text');
+    let header = document.querySelector('header');
+    let headerTop = header.querySelector('.header_top');
+    let headerContent = header.querySelector('.header_content');
+    let aboutMe = document.querySelector('.aboutMe');
+    let skills = document.querySelector('.skills');
+    let skillsText = skills.querySelector('.skills_text');
+    let projects = document.querySelector('.projects');
 
 
         $(window).scroll(function () {
-            var long = $(window).scrollTop();
+            let long = $(window).scrollTop();
             if (long > headerContent.offsetTop+5) {
                 headerContent.classList.add('header_fixed');
-                console.log(skills.offsetTop + skills.offsetHeight);
             } else {
                 headerContent.classList.remove('header_fixed');
             }
@@ -22,5 +23,24 @@
                  skillsText.classList.add('skills_text_animation');
             }
         });
+     
+
+    
+    let siteNav = function (etc, content){
+        $(etc).click(function () {
+            console.log(content.offsetTop + ' ' + header.offsetHeight + ' ' + headerTop.offsetHeight);
+            let b = content.offsetTop - header.offsetHeight - headerTop.offsetHeight + 10;
+            window.scrollTo(0, b);
+            
+        });    
+    };
+    
+    
+    siteNav('#aboutMe', aboutMe);
+    siteNav('#skills', skills);
+    siteNav('#projects', projects);
+    siteNav('#contacts', skills);
+
+    
     
 })();
